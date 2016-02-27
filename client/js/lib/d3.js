5e2f96a4c2f5bfd849f2b63b8b1cab4430a27b9a
Ada Rose <ada.edwards@ft.com>
2016-02-26 17:54:44 -0800

    Initial commit

---
+++ client/js/lib/d3.js
@@ -0,0 +1,108 @@
+/* global d3 */
+/**
+ * Based on http://bl.ocks.org/MoritzStefaner/1377729
+ */
+
+'use strict';
+
+module.exports = function ({
+	data,
+	width,
+	height
+}) {
+
+	width = width || 400;
+	height = height || 400;
+	const nodes = data.slice(0);
+	const ringSize = height / 10;
+
+	const links = nodes.map((n, i) => ({
+		target: 0,
+		source: i + 1,
+		distance: (10 - n['do-able']) * ringSize
+	}));
+
+	nodes.unshift({
+		name: 'root',
+		x: width,
+		y: height,
+		fixed: true,
+		visible: false,
+		rootEl: true
+	});
+
+	/**
+	 * constiables
+	 */
+
+	const svgNode = document.createElementNS(d3.ns.prefix.svg, 'svg');
+	const svg = d3.select(svgNode)
+		.attr('width', width)
+		.attr('height', height);
+
+	const force = d3.layout.force()
+		.nodes(nodes)
+		.links(links)
+		.gravity(0.1)
+		.charge(-100)
+		.chargeDistance(150)
+		.linkStrength(20)
+		.linkDistance(l => l.distance)
+		.size([width, height]);
+
+	force.on('tick', function () {
+		node.attr('transform', d => `translate(${d.x}, ${d.y})`);
+	});
+
+	const node = svg.selectAll('.node')
+		.data(nodes)
+		.enter()
+		.append('svg:g')
+		.attr('class', d => d.rootEl ? 'rootNode' : 'node');
+
+	node
+		.style('display', d => d.visible === false ? 'none' : 'initial');
+
+	node.append('circle')
+		.attr('class', 'node')
+		.attr('r', 8)
+		.style('fill', 'white')
+		.style('stroke-width', '2px')
+		.style('stroke', 'black');
+
+	node.append('svg:text')
+		.text(d => d.name || '')
+		.attr('class', 'd3-label');
+
+	const gradient = svg.append('svg:defs')
+		.append('svg:radialGradient')
+		.attr('id', 'radgrad')
+		.attr('x1', '0%')
+		.attr('y1', '0%')
+		.attr('x2', '100%')
+		.attr('y2', '100%')
+		.attr('spreadMethod', 'pad');
+
+	// Define the gradient colors
+	gradient.append('svg:stop')
+		.attr('offset', '90%')
+		.attr('stop-color', 'rgba(255, 255, 255, 0)')
+		.attr('stop-opacity', 1);
+
+	gradient.append('svg:stop')
+		.attr('offset', '100%')
+		.attr('stop-color', '#CCCCFF')
+		.attr('stop-opacity', 1);
+
+	const rootNode = svg.select('.rootNode');
+	for (let i=0; i<10; i++) {
+		rootNode.append('circle')
+			.attr('class', 'node')
+			.attr('r', (10 - i) * ringSize)
+			.style('fill', `url(#radgrad)`);
+	}
+
+	force.start().alpha(0.1);
+
+	return svgNode;
+};
---
+++ client/js/main.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const graph = require('./lib/d3');
+const berthaRoot = 'http://bertha.ig.ft.com/';
+const berthaView = 'view/publish/gss/';
+const berthaRepublish = 'republish/publish/gss/';
+const dataUrlFragment = (function () {
+	const queryString = require('query-string');
+	const parsed = queryString.parse(location.search);
+	if (parsed.id && parsed.sheet) {
+		return `${parsed.id}/${parsed.sheet}`;
+	}
+	throw Error('No ID and Sheet parameters.');
+}());
+
+
+function addScript (url) {
+	return new Promise(function (resolve, reject) {
+		const script = document.createElement('script');
+		script.setAttribute('src', url);
+		document.head.appendChild(script);
+		script.onload = resolve;
+		script.onerror = reject;
+	});
+}
+
+function generateData (data) {
+	data = data.filter(datum => !!datum['do-able'] && !!datum['name']);
+
+	const svgTarget = document.getElementById('tech-radar__graph-target');
+	const svg = graph({
+		data,
+		width: svgTarget.clientWidth,
+		height: svgTarget.clientWidth*0.66
+	});
+	const table = document.createElement('table');
+	const thead = document.createElement('thead');
+	const theadTr = document.createElement('tr');
+	const tbody = document.createElement('tbody');
+	let headings = new Set();
+	data.forEach(datum => {
+		const keys = Object.keys(datum);
+		keys.forEach(k => headings.add(k));
+	});
+
+	table.appendChild(thead);
+	thead.appendChild(theadTr);
+	table.appendChild(tbody);
+	table.classList.add('filter-table');
+	for (const heading of headings.values()) {
+		const td = document.createElement('td');
+		td.textContent = heading;
+		theadTr.appendChild(td);
+	}
+	for (const datum of data) {
+		const tbodyTr = document.createElement('tr');
+		tbody.appendChild(tbodyTr);
+		tbodyTr.classList.add('collapsed');
+		for (const heading of headings.values()) {
+			const td = document.createElement('td');
+			const tdContent = document.createElement('div');
+			tdContent.classList.add('datum');
+			tdContent.textContent = datum[heading] || '';
+			td.classList.add(heading);
+			td.appendChild(tdContent);
+			tbodyTr.appendChild(td);
+		}
+	}
+	document.getElementById('tech-radar__filter-list-target').appendChild(table);
+	svgTarget.appendChild(svg);
+
+	return function cleanUp () {
+		table.parentNode.removeChild(table);
+		svg.parentNode.removeChild(svg);
+	};
+}
+
+Promise.all([
+	addScript('https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js'),
+	addScript('https://polyfill.webservices.ft.com/v1/polyfill.min.js?features=fetch,default')
+])
+.then(() => dataUrlFragment)
+.then(urlFragment => `${berthaRoot}${berthaView}${urlFragment}`)
+.then(fetch)
+.then(response => response.json())
+.then(function (data) {
+
+	let cleanUp = generateData(data);
+
+	const buttons = document.getElementById('tech-radar__buttons');
+
+	const updateDataButton = document.createElement('button');
+	updateDataButton.textContent = 'Refresh Data';
+	buttons.appendChild(updateDataButton);
+	updateDataButton.addEventListener('click', () => {
+		cleanUp();
+		fetch(`${berthaRoot}${berthaRepublish}${dataUrlFragment}`)
+		.then(response => response.json())
+		.then(data => {
+			cleanUp = generateData(data);
+		});
+	})
+	(data);
+});
