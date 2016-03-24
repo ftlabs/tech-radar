af55141b427c2ca8a88a1a61967fb372fbd445fb
Ada Rose Edwards <ada@ada.is>
2016-03-23 19:40:40 -0700

    install bower after build

--- bower_components/o-tracking/docs/ftcom_example.md
+++
@@ -1,3 +0,0 @@
-# o-tracking for Live Pub
-
-[Google doc](https://drive.google.com/open?id=1Y30obt8AeeohLvTA748sXjYYwwSboKOmdxhhGMDxQys)
--- bower_components/o-tracking/src/javascript/core/transports/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	xhr: require('./xhr'),
-	sendBeacon: require('./send-beacon'),
-	image: require('./image'),
-	get: function (name) {
-		return this.mock || this[name];
-	}
-}
